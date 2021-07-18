9c3be3295349e5ea5fdda6d2563b9acbc10f3d0f
steebe <steve@stevebass.me>
2021-07-17 20:22:54 -0700

    Making the script a bit more robust. TODO: world names as args

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Valheim save file(s).
+# valheim
+The alternative to Valheim server state sharing you all have been waiting for!
+
+## Usage
+
+```bash
+$ git clone git@github.com:steebe/valheim.git
+$ cd valheim
+$ ./copy-to-worlds.sh <WINDOWS_USERNAME>
+```
+
+The above will make a backup of your existing _worlds_ state, which contains **all** of your Valheim saves, and then create a new or overwrite the existing `TheTommune` server world.
--- copy-to-worlds.sh
+++ copy-to-worlds.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
+# TODO: Allow for the world names to be arguments to avoid hard-coding and expand
+# the repo capabilities beyond me & Tom :) 
+
+# TODO: Allow for timestamp checks against the file contents of the worlds being 
+# copied in, and prevent any unnecessary overwrites.
+
+TOMMUNE_NAME="TheTommune"
+TOMMUNE_SOURCE="./$TOMMUNE_NAME/*"
+
+# Validate user input
 USERNAME=$1
 
-cp ./TheTommune/* /C/Users/$1/AppData/LocalLow/IronGate/Valheim/worlds/
+if [ -z "$USERNAME" ]
+then
+	echo " >> \$USERNAME must be provided to access Save Data"
+	echo " >> EXITING"
+	exit
+fi
+
+# Validate local Valheim saves exist
+
+VALHEIM_WORLDS_DIR=/C/USERS/$USERNAME/AppData/LocalLow/IronGate/Valheim/worlds/
+if [ ! -d "$VALHEIM_WORLDS_DIR" ]
+then
+	echo " >> No Valheim worlds directory present."
+	echo " >> EXITING"
+	exit
+fi
+
+VALHEIM_WORLDS_TAR=/C/Users/$USERNAME/AppData/LocalLow/IronGate/Valheim/worlds.tar
+
+echo " >> Backing up 'worlds/' to $VALHEIM_WORLDS_TAR"
+tar -zcvf $VALHEIM_WORLDS_TAR $VALHEIM_WORLDS_DIR
+
+echo " >> Overwriting $TOMMUNE_NAME Save Data"
+cp ./$TOMMUNE_NAME/* /C/Users/$USERNAME/AppData/LocalLow/IronGate/Valheim/worlds/
